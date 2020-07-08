10ca6ffc4a517af721e471b30076a2d50d2d4e50
Ilm <MegaAwokenIlm@gmail.com>
2020-07-07 20:21:40 -0700

    Updates

---
+++ precache-manifest.19d65440e9b2c98743abda37adb04335.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db368871ecd35e9796a1b53b25388a84",
+    "url": "/slideshow/index.html"
+  },
+  {
+    "revision": "b7cf57be65b6d808550b",
+    "url": "/slideshow/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db8a9a0e069e0e240d4c",
+    "url": "/slideshow/static/js/2.40c1d768.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slideshow/static/js/2.40c1d768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7cf57be65b6d808550b",
+    "url": "/slideshow/static/js/main.45a7241a.chunk.js"
+  },
+  {
+    "revision": "0576e9a8e760b5340d74",
+    "url": "/slideshow/static/js/runtime-main.b8f47591.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/slideshow/static/media/logo.5d5d9eef.svg"
+  }
+]);
