b5431c45469ae6a8cec2762679b3bb97054e263a
Ilm <MegaAwokenIlm@gmail.com>
2020-12-13 10:59:03 -0800

    Updates to E621Settings

--- src/components/E621Settings.js
+++ src/components/E621Settings.js
@@ -1,32 +1,65 @@
 import React from 'react';
+import Authentication from '../helpers/Authentication';
+
+import Field from './Field';
 
 
 class E621Settings extends React.Component {
   constructor(props) {
     super(props);
-    this.handleTagsChange = this.handleTagsChange.bind(this);
-    this.state = {tags: ''};
+    this.handleLocalChange = this.handleLocalChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSaveLogin = this.handleSaveLogin.bind(this);
+    this.state = {
+      username: '',
+      apiKey: '',
+      userBlacklist: true,
+      avoidAnonBlacklist: false,
+      setID: '',
+    };
   }
 
   render() {
     return (
       <div className="form">
-        <input id="e621-username" type="text" />
-        <input id="e621-api-key" type="password" />
-        <button id="e621-apply-button">Apply</button>
-        <button id="e621-save-login-button">Store Login Locally</button>
+        <Field label="Username" type="text" 
+               name="username" value={this.state.username}
+               onChange={this.handleLocalChange} />
+        <Field label="API Key" type="password" 
+               name="apiKey" value={this.state.apiKey}
+               onChange={this.handleLocalChange} />
+        <button id="e621-save-login-button" onClick={this.handleSaveLogin}>
+          Store Login Locally
+        </button>
+        <Field label="User Blacklist" type="checkbox" 
+               name="userBlacklist" checked={this.state.userBlacklist}
+               onChange={this.handleChange} />
+        <Field label="Avoid Anon Blacklist" type="checkbox" 
+               name="avoidAnonBlacklist" checked={this.state.avoidAnonBlacklist}
+               onChange={this.handleChange} />
+        <Field label="Set ID" type="text" 
+               name="setID" value={this.state.setID}
+               onChange={this.handleChange} />
       </div>
     );
   }
 
-  handleTagsChange(e) {
-    this.setState({ tags: e.target.value });
+  handleLocalChange(event) {
+    let target = event.target;
+    let value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({[target.name]: value});
   }
 
-  handleTagsKey(e) {
-    if (e.key === 'Enter') {
-      this.serachCallback();
-    }
+  handleChange(event) {
+    let target = event.target;
+    let value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({[target.name]: value});
+    this.props.onSettingsChange({[target.name]: value});
+  }
+
+  handleSaveLogin() {
+    Authentication.storeLogin(this.state.username, this.state.apiKey);
+    this.setState({ username: '', apiKey: '' });
   }
 }
 
