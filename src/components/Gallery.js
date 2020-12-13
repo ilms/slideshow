fa9619daa39a4443c84418f81ebb93454ecdf2d0
Ilm <MegaAwokenIlm@gmail.com>
2020-12-13 10:53:37 -0800

    Added Gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,31 +1,47 @@
 import React from 'react';
-import { Slideshow } from './Slideshow';
-import { E621Filters } from './E621Filters';
+
+import Card from './Card';
+
+import './Gallery.css';
 
 
-class Gallery extends React.component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mode: 'search',
-      filters: {},
-      settings: {},
-    };
-  }
-
+class Gallery extends React.Component {
   render() {
-    switch (this.state.mode) {
-      default:
-      case 'search':
-        return (
-          <E621Filters searchCallback={this.search.bind(this)} />
-        );
-      case 'gallery':
-        return 
-    }
+    return (
+      <div className="gallery">
+        <div className="gallery-controls">
+          <button onClick={this.props.openSearch}>Search</button>
+          <button onClick={this.props.openSettings}>Settings</button>
+        </div>
+        <div className="gallery-controls">
+          <button onClick={this.props.prevPage} disabled={!this.props.prevPage}>Prev Page</button>
+          <button onClick={this.props.nextPage} disabled={!this.props.nextPage}>Next Page</button>
+        </div>
+        <div className="gallery-content">
+          {this.getCards()}
+        </div>
+        <div className="gallery-controls">
+          <button onClick={this.props.prevPage} disabled={!this.props.prevPage}>Prev Page</button>
+          <button onClick={this.props.nextPage} disabled={!this.props.nextPage}>Next Page</button>
+        </div>
+      </div>
+    );
   }
 
-  search() {
-
+  getCards() {
+    let cards = [];
+    if (this.props.postManager) {
+      let posts = this.props.postManager.getAllPosts();
+      for (let i = 0; i < posts.length; i++) {
+        cards.push(
+          <Card key={posts[i].id}
+                post={posts[i]} 
+                callback={() => {this.props.viewPost(i)}} />
+        );
+      }
+    }
+    return cards;
   }
 }
+
+export default Gallery
