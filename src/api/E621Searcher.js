b21ede4bc8ed92fc810fa6a9f8a1f6b2151e83d4
Ilm <MegaAwokenIlm@gmail.com>
2020-12-13 10:55:38 -0800

    Updated and changed E621Fetcher to E621Searcher

---
+++ src/api/E621Searcher.js
@@ -0,0 +1,59 @@
+import E621API from './E621API';
+import Post from './Post';
+import ThrottleQueue from '../helpers/ThrottleQueue';
+
+
+/**
+ * This class 
+ */
+export default class E621Searcher {
+  static queue = new ThrottleQueue(1000);
+
+  constructor(tags) {
+    this.tags = tags;
+    this.limit = 50;
+    this._page = 1;
+    this._fetching = false;
+    this._finished = false;
+  }
+
+  fetch(callback) {
+    if (this._finished === true) {
+      setTimeout(() => { callback([]); }, 0);
+      return true;
+    } else if (this._fetching === true) {
+      return false;
+    } else {
+      this._fetching = true;
+      let request = {
+        tags: this.tags,
+        page: this._page,
+        limit: this.limit,
+        success: this.fetchSuccess.bind(this, callback),
+      }
+      E621Searcher.queue.add(function() {
+        E621API.getPosts(request);
+      });
+      this._page++;
+      return true;
+    }
+  }
+
+  fetchSuccess(callback, data) {
+    this._fetching = false;
+    if (data['posts'].length === 0) {
+      this._finished = true;
+      callback([]);
+    } else {
+      let posts = [];
+      for (let i = 0; i < data['posts'].length; ++i) {
+        posts.push(new Post(data['posts'][i]));
+      }
+      callback(posts);
+    }
+  }
+
+  get finished() {
+    return this._finished;
+  }
+}
