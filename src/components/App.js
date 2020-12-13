a4b31ed866f6668b3929efce9052049bdb9bcac5
Ilm <MegaAwokenIlm@gmail.com>
2020-12-13 10:58:33 -0800

    Various updates to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,69 +1,67 @@
 import React from 'react';
 
-import E621Fetcher from '../api/E621Fetcher';
-import Post from '../api/Post';
+import E621API from '../api/E621API';
+import E621Searcher from '../api/E621Searcher';
 import E621Filters from './E621Filters';
+import E621Settings from './E621Settings';
+import Gallery from './Gallery';
 import ImageCache from './ImageCache';
 import Slideshow from './Slideshow';
 import PostManager from '../helpers/PostManager';
 
-import missing from '../missing.png';
+import './App.css';
 
 
 class App extends React.Component {
-  static missingPost = new Post(
-    0,
-    missing,
-    missing,
-    "png",
-    "#"
-  );
-
   constructor(props) {
     super(props);
     this.search = this.search.bind(this);
     this.searchCallback = this.searchCallback.bind(this);
-    this.searchEscape = this.searchEscape.bind(this);
-    this.slideshowEscape = this.slideshowEscape.bind(this);
+    this.openSlideshow = this.openSlideshow.bind(this);
+    this.openGallery = this.openGallery.bind(this);
     this.slideshowNext = this.slideshowNext.bind(this);
     this.slideshowPrev = this.slideshowPrev.bind(this);
     this.keydownHandler = this.keydownHandler.bind(this);
+    this.openSearch = this.openSearch.bind(this);
+    this.galleryViewPost = this.galleryViewPost.bind(this);
+    this.addToSet = this.addToSet.bind(this);
+    this.onSettingsChange = this.onSettingsChange.bind(this);
     this.state = {
       mode: 'search',
       api: 'e621',
       settings: {},
       currentCache: [],
       slideshowPost: null,
+      posts: [],
       postManager: null,
     };
   }
 
   render() {
-    switch (this.state.mode) {
-      default:
-      case 'search':
-        switch(this.state.api) {
-          default:
-          case 'e621':
-            return (
-              <>
-                <ImageCache posts={this.state.currentCache} />
-                <E621Filters searchCallback={this.search} 
-                            escape={this.searchEscape} />
-              </>
-            );
-        }
-      case 'slideshow':
-        return (
-          <>
-            <ImageCache posts={this.state.currentCache} />
-            <Slideshow post={this.state.slideshowPost || App.missingPost}
-                      next={this.slideshowNext}
-                      prev={this.slideshowPrev}
-                      escape={this.slideshowEscape} />
-          </>
-        );
-    }
+    return (
+      <>
+        <div className={this.state.mode === 'search' ? 'mount show' : 'mount'}>
+          <E621Filters searchCallback={this.search} 
+                       escape={this.openGallery} />
+        </div>
+        <div className={this.state.mode === 'slideshow' ? 'mount show' : 'mount'}>
+          <Slideshow post={this.state.slideshowPost}
+                     next={this.slideshowNext}
+                     prev={this.slideshowPrev}
+                     escape={this.openGallery}
+                     addToSet={this.addToSet} />
+        </div>
+        <div className={this.state.mode === 'gallery' ? 'mount show' : 'mount'}>
+          <Gallery postManager={this.state.postManager}
+                   openSearch={this.openSearch}
+                   viewPost={this.galleryViewPost} />
+        </div>
+        <div className={this.state.mode === 'settings' ? 'mount show' : 'mount'}>
+          <E621Settings onSettingsChange={this.onSettingsChange} />
+        </div>
+        <ImageCache posts={this.state.currentCache} />
+      </>
+    );
   }
 
   keydownHandler(e) {
@@ -89,54 +87,80 @@ class App extends React.Component {
   }
 
   componentWillUnmount() { 
-    document.addEventListener('keydown', this.keydownHandler);
+    document.removeEventListener('keydown', this.keydownHandler);
   }
 
   search(filters) {
     // TODO pass proper settings
     let settings = {limit: 50};
-    let postManager = new PostManager(new E621Fetcher(filters, settings));
+    let postManager = new PostManager(new E621Searcher(filters.tags, settings.limit));
     postManager.fetchIfApplicable(this.searchCallback);
     this.setState({
-      mode: 'slideshow',
+      mode: 'gallery',
       postManager: postManager,
     });
   }
 
-  searchCallback() {
+  update() {
+    // TODO configure cache size with settings
     this.setState({
-      mode: 'slideshow',
+      posts: this.state.postManager.getAllPosts(),
       currentCache: this.state.postManager.getCachePosts(10, 5),
       slideshowPost: this.state.postManager.getCurrentPost(),
     });
   }
 
-  searchEscape() {
+  searchCallback() {
+    this.update();
+  }
+
+  openSearch() {
+    this.setState({
+      mode: 'search',
+    })
+  }
+
+  openSlideshow() {
     this.setState({
       mode: 'slideshow',
     });
   }
 
-  slideshowEscape() {
+  openGallery() {
     this.setState({
-      mode: 'search',
+      mode: 'gallery',
     });
   }
 
   slideshowNext() {
     this.state.postManager.next();
-    // TODO configure cache size with settings
-    this.setState({
-      currentCache: this.state.postManager.getCachePosts(10, 5),
-      slideshowPost: this.state.postManager.getCurrentPost(),
-    });
+    this.update();
   }
 
   slideshowPrev() {
     this.state.postManager.prev();
+    this.update();
+  }
+
+  galleryViewPost(index) {
+    this.state.postManager.moveTo(index);
+    this.update();
     this.setState({
-      currentCache: this.state.postManager.getCachePosts(10, 5),
-      slideshowPost: this.state.postManager.getCurrentPost(),
+      mode: 'slideshow',
+    });
+  }
+
+  addToSet() {
+    let post = this.state.postManager.getCurrentPost();
+    E621API.addPostToSet({
+      post_id: post.id,
+      set_id: this.settings.setID
+    });
+  }
+
+  onSettingsChange(settings) {
+    this.setState((state) => {
+      return {settings: {...state.settings, ...settings}};
     });
   }
 }
