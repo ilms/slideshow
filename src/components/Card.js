5c430d48ae0d7023d891004fcbe61bfd05f63473
Ilm <MegaAwokenIlm@gmail.com>
2020-12-14 18:53:22 -0800

    Made Cards smaller and added WEBM label

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,10 @@ import './Card.css';
 
 class Card extends React.Component {
   render() {
+    let className = 'card';
+    className += ' content-type-' + this.props.post.fileExtension;
     return (
-      <div className="card">
+      <div className={className}>
         <button onClick={this.props.callback}>
           <img src={this.props.post.previewURL} alt="" />
         </button>
